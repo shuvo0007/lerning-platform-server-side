3b88e4b59ab1d10cde4c06adb05e3df7099f0e11
Md. Samiul Islam <samiul15-6903@diu.edu.bd>
2022-10-26 01:25:18 +0600

    category edited

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 const category = require("./data/category.json");
+const courses = require("./data/courses.json");
 
 app.get("/", (req, res) => {
   res.send("Data running");
 });
+
 app.get("/category", (req, res) => {
   res.send(category);
+});
+
+
+app.get("/courses/:id", (req, res) => {
+    const id = req.params.id;
+  const course = courses.filter((n) => n.category_id === id);
+  res.send(course);
 });
 
 app.listen(port, () => {

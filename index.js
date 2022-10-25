017363e0f7a15305d9771c52d87388b91c293c62
Md. Samiul Islam <samiul15-6903@diu.edu.bd>
2022-10-25 23:23:12 +0600

    courses update

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Digital Learning API running');
+app.use(cors());
+const category = require("./data/category.json");
+
+app.get("/", (req, res) => {
+  res.send("Data running");
+});
+app.get("/category", (req, res) => {
+  res.send(category);
 });
 
 app.listen(port, () => {
-    console.log('Digital Learning server running on port', port);
-})
+  console.log("Digital Learning server running on port", port);
+});
